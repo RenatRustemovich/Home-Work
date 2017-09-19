0185ac37fd443404100afb38bbd51d9f1cc1d79e
Sabirov Renat 11-704 <mr.cabir1999@mail.ru>
2017-09-19 10:55:46 +0300

    commit

--- Задачи/1/colodec.java
+++ Задачи/1/colodec.java
@@ -1,14 +1,21 @@
-import java.util.Scanner;
+
 
 public class colodec{
 
 	public static void main (String [] args){
-
-		Scanner in = new Scanner(System.in);
-		double h;
-		double g = 9.8f;
-		double t = in.nextDouble();
-		h = g*t*t/2;
-		System.out.println(h);
+		if (args.length < 1){
+			System.err.println("Please enter data");
+			System.exit(1);
+			} else {
+			  	double t = Double.parseDouble(args[0]);
+				if (t<0){
+						System.err.println("Please try again");
+				} else {
+						double h;
+						double g = 9.8f;
+						h = g*t*t/2;
+						System.out.println(h);}
+		}
 	}
+
 }
--- Задачи/2/Circle.java
+++ Задачи/2/Circle.java
@@ -1,17 +1,18 @@
 import java.util.Scanner;
 
-public class Vallis{
+public class Circle{
 
 	public static void main (String[] args){
 
-		System.out.println("Введите колличеств множителей: ");
+		System.out.println("Введите радиус: ");
 		Scanner in = new Scanner(System.in);
-		Int num = in.nextInt();
-		double pi = 1;
-		for (int i = 1; i <= num; i++) {
-      		pi = (pi*(i*2)*(i*2))\((i*2-1)*(i*2+1));
-    		}
-		
-		System.out.println("Число ПИ: "+pi);
+		double R  = in.nextDouble();
+		double l;
+		double S;
+		S = Math.PI*R*R;
+		l = 2*Math.PI*R;
+		System.out.println("S = "+S);
+		System.out.println("l ="+l);
 	}
+
 }
